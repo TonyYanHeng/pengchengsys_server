c8ea814092a3d133cb09abf39f808e016b347dee
TonyYanHeng <18030606135@163.com>
2019-04-22 15:49:59 +0800

    用户登录和注册功能实现--界面html和css

--- pengchengsys/login_register/views.py
+++ pengchengsys/login_register/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def login(request):
-    return render(request, 'login_register/login.html')
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'login_register/login.html', context)
+    elif request.method == 'POST':
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        user = authenticate(request, username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login_register/home.html')
+        else:
+            context["err_info"] = "用户名和密码不正确！"
+            return render(request, 'login_register/login.html', context)
 
 
 def register(request):
